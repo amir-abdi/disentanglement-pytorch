ab97d97311a29a02d05abb462d4d98f783696a13
amirabdi <amir.h.abdi@gmail.com>
2019-06-10 15:25:47 -0700

    Update dsprites_ae.sh

--- scripts/dsprite_ae.sh
+++ scripts/dsprite_ae.sh
@@ -9,12 +9,14 @@ echo "name=$NAME"
 python3 main.py \
 --name=$NAME \
 --alg=AE \
---dset_dir=/home/amirabdi/data/Datasets/ \
+--dset_dir=$DATASETS \
 --dset_name=dsprites \
 --encoder=SimpleEncoder64 \
 --decoder=SimpleDecoder64 \
 --z_dim=8 \
+--w_recon=10000 \
 --use_wandb=false \
+--ckpt_load=./saved_models/dsprite_ae/last
 
 
 
