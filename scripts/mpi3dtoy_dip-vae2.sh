1fdb1cf0208bc1ce566d18a50cb6bd43c8bc7712
amirabdi <amir.h.abdi@gmail.com>
2019-07-22 00:56:11 -0700

    Update scripts to include pretrained model

--- scripts/mpi3dtoy_dip-vae2.sh
+++ scripts/mpi3dtoy_dip-vae2.sh
@@ -10,21 +10,18 @@ python3 main.py \
 --aicrowd_challenge=true \
 --name=$NAME \
 --alg=BetaVAE \
---vae_loss=AnnealedCapacity \
---vae_type=FactorVAE \
---dset_dir=$DISENTANGLEMENT_LIB_DATA \
---dset_name=$AICROWD_DATASET_NAME \
+--vae_loss=Basic \
+--vae_type=DIPVAE \
+--dip_type=i \
 --traverse_z=true \
 --encoder=PadlessGaussianConv64 \
 --decoder=SimpleConv64 \
 --discriminator=SimpleDiscriminator \
 --z_dim=20 \
---use_wandb=true \
---w_kld=1.0 \
---w_tc=1.0 \
---lr_G=0.0008 \
+--use_wandb=True \
+--w_kld=1 \
+--lr_G=0.0005 \
 --max_iter=30000 \
---iterations_c=20000 \
 --ckpt_load=./saved_models/mpi3dtoy_ae_gaussian/saved2 \
 --ckpt_load_iter=false \
 
