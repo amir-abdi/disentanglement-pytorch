227483af6a7487770d6d864a340c7e37dd36a9ae
amirabdi <amir.h.abdi@gmail.com>
2019-07-27 22:39:41 -0700

    1.9 bigger batch, bigger w_tc

--- run.sh
+++ run.sh
@@ -36,7 +36,8 @@ export PYTHONPATH=${PYTHONPATH}:${NDC_ROOT}
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-saved4.sh
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-saved4-beta.sh
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_factor-betatc-vae-saved4.sh
-bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-fromCeleb.sh 
+#bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-fromCeleb.sh 
+bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-1.9-try1.sh
 
 #--ckpt_load=/home/amirabdi/disentanglement-pytorch/checkpoints/last \
 
