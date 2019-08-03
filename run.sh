16c16c9feebd939eeede143d17ce5dad73d8f542
amirabdi <amir.h.abdi@gmail.com>
2019-08-03 04:51:41 -0700

    v153

--- run.sh
+++ run.sh
@@ -41,5 +41,5 @@ export PYTHONPATH=${PYTHONPATH}:${NDC_ROOT}
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-1.9-try2.sh
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-1.9-try3.sh
 #bash ${NDC_ROOT}/scripts/mpi3dtoy_betatc-vae-1.9-try11.sh
-bash ${NDC_ROOT}/scripts/mpi3dtoy_v156.sh
+bash ${NDC_ROOT}/scripts/mpi3dtoy_v153.sh
 
