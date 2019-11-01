6d242b3fe7f12e7d78f9b7c795da2ce0ec44ab69
amirabdi <amir.h.abdi@gmail.com>
2019-10-31 20:35:41 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,13 @@ echo NDC_ROOT=$NDC_ROOT
 
 # Source the training environment (see the env variables defined therein) if we are not evaluating
 if [ ! -n "${AICROWD_IS_GRADING+set}" ]; then
-  echo "AICROWD_IS_GRADING is not set and you're running locally... so, exit and run your favourite script "\
-  "from the scripts/ directory!"
+  echo "AICROWD_IS_GRADING is not set and you're running locally."
+  echo "This script (run.sh) is designed for runs on the AIcrowd server."
+  echo "Please run your favourite script from the scripts/ directory"
   exit 1
 else
   # We're on the AICrowd evaluator. Add root to python path
   export PYTHONPATH=${PYTHONPATH}:${NDC_ROOT}
 fi
 
-bash ${NDC_ROOT}/scripts/aicrowd_script.sh
+bash ${NDC_ROOT}/scripts/aicrowd_challenge.sh
