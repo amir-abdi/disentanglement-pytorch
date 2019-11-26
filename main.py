f0a3f29b32b1a11fe4594386254a3ffd6063f176
amirabdi <amir.h.abdi@gmail.com>
2019-11-26 10:51:59 -0800

    Refactor main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
 
 
-def main(args):
+def main(_args):
     on_aicrowd_server = is_on_aicrowd_server()
 
     if on_aicrowd_server:
@@ -21,30 +21,30 @@ def main(args):
         aicrowd_helpers.register_progress(0.)
 
         # turn off all logging and visualization
-        args.use_wandb = False
-        args.all_iter = args.max_iter + 1
+        _args.use_wandb = False
+        _args.all_iter = _args.max_iter + 1
 
     # load the model associated with args.alg
-    model_cl = getattr(models, args.alg)
-    model = model_cl(args)
+    model_cl = getattr(models, _args.alg)
+    model = model_cl(_args)
 
     # load checkpoint
-    if args.ckpt_load:
-        model.load_checkpoint(args.ckpt_load, load_iternum=args.ckpt_load_iternum, load_optim=args.ckpt_load_optim)
+    if _args.ckpt_load:
+        model.load_checkpoint(_args.ckpt_load, load_iternum=_args.ckpt_load_iternum, load_optim=_args.ckpt_load_optim)
 
     # run test or train
-    if not args.test:
+    if not _args.test:
         model.train()
     else:
         model.test()
 
     # if this is part of the aicrowd_challenge, export and inference model and run disentanglement evaluation
-    if args.aicrowd_challenge:
+    if _args.aicrowd_challenge:
         from aicrowd import utils_pytorch as pyu, aicrowd_helpers
         # Export the representation extractor
         path_to_saved = pyu.export_model(pyu.RepresentationExtractor(model.model.encoder, 'mean'),
                                          input_shape=(1, model.num_channels, model.image_size, model.image_size))
-        logging.info('A copy of the model saved in {}'.format(path_to_saved))
+        logging.info(f'A copy of the model saved in {path_to_saved}')
 
         if on_aicrowd_server:
             # AICrowd will handle the evaluation
@@ -59,13 +59,13 @@ def main(args):
 
 
 if __name__ == "__main__":
-    args = get_args(sys.argv[1:])
-    setup_logging(args.verbose)
-    initialize_seeds(args.seed)
+    _args = get_args(sys.argv[1:])
+    setup_logging(_args.verbose)
+    initialize_seeds(_args.seed)
 
     # set the environment variables for dataset directory and name, and check if the root dataset directory exists.
-    set_environment_variables(args.dset_dir, args.dset_name)
+    set_environment_variables(_args.dset_dir, _args.dset_name)
     assert os.path.exists(os.environ.get('DISENTANGLEMENT_LIB_DATA', '')), \
-        'Root dataset directory does not exist at: \"{}\"'.format(args.dset_dir)
+        'Root dataset directory does not exist at: \"{}\"'.format(_args.dset_dir)
 
-    main(args)
+    main(_args)
